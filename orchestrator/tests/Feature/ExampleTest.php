b39ec112ac40c983a8965ebd4abc53e14374b950
Markus Heinemann <git@heinemann.dev>
2026-04-14 21:49:56 +0200

    add inertia react setup

--- orchestrator/tests/Feature/ExampleTest.php
+++ orchestrator/tests/Feature/ExampleTest.php
@@ -1,7 +1,12 @@
 <?php
 
+use Inertia\Testing\AssertableInertia;
+
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertStatus(200)
+        ->assertInertia(function (AssertableInertia $page) {
+            $page->component('index');
+        });
 });
